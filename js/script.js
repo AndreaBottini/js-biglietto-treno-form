fef198ef2a05e250a69863878da0ab9ac1e65b3a
Andrea Bottini <andrea.bottini22@gmail.com>
2019-12-19 20:17:04 +0100

    Termine parte JS

--- js/script.js
+++ js/script.js
@@ -26,44 +26,53 @@ buttonGenera.addEventListener ('click',
     var fascia = fasciaEtaSelect.value;
     console.log(fascia);
 
-    // // Quanti anni ha l'utente?
-    // var etaUtente = parseInt(prompt('Ora avremmo bisogno di conoscere la tua età'));
-    // console.log(etaUtente);
-    //
+
     // // è necessario definire il costo del biglietto in base ai chilometri che l'utente percorre
     // // Se gli utenti hanno una età compresa tra i 18 anni e i 64 anni andrà applicata la tariffa normale
-    // var calcoloViaggio = chilometriUtente * 0.21;
-    // // console.log(calcoloViaggio);
-    //
-    // // Se l'utente è minorenne andrà applicato uno sconto del 20%
-    // var calcoloViaggio20 = (Math.round (chilometriUtente * 0.21) / 100) * 20;
-    // // console.log(calcoloViaggio20);
-    //
-    // // Se l'utente è over 65 andrà applicato uno sconto del 40%
-    // var calcoloViaggio40 = (Math.round (chilometriUtente * 0.21) / 100) * 40;
-    // // console.log(calcoloViaggio40);
-    //
-    // var calcoloViaggioSconto20 = Math.round (calcoloViaggio - calcoloViaggio20);
-    // // console.log(calcoloViaggioSconto20);
-    //
-    // var calcoloViaggioSconto40 = Math.round (calcoloViaggio - calcoloViaggio40);
-    // // console.log(calcoloViaggioSconto40);
-    //
-    // if (etaUtente < 18) {
-    //
-    //   calcoloViaggioSconto20 = calcoloViaggio - calcoloViaggio20;
-    //   console.log(calcoloViaggioSconto20);
-    // }
-    // else if (etaUtente >= 65) {
-    //
-    //   calcoloViaggioSconto40 = calcoloViaggio - calcoloViaggio40;
-    //   console.log(calcoloViaggioSconto40);
-    // }
-    // else {
-    //
-    //   calcoloViaggio
-    //   console.log(calcoloViaggio)
-    // }
+    var calcoloViaggio = chilometriUtente * 0.21;
+    console.log(calcoloViaggio);
 
+    // Se l'utente è minorenne andrà applicato uno sconto del 20%
+    var calcoloViaggio20 = chilometriUtente * 0.21 / 100 * 20;
+    // console.log(calcoloViaggio20);
+
+    // Se l'utente è over 65 andrà applicato uno sconto del 40%
+    var calcoloViaggio40 = chilometriUtente * 0.21 / 100 * 40;
+    // console.log(calcoloViaggio40);
+
+    var calcoloViaggioSconto20 = calcoloViaggio - calcoloViaggio20;
+    // console.log(calcoloViaggioSconto20);
+
+    var calcoloViaggioSconto40 = calcoloViaggio - calcoloViaggio40;
+    // console.log(calcoloViaggioSconto40);
+
+    var offerta = 'Tariffa Standard';
+
+
+    if (fascia == 'Minorenne') {
+
+      calcoloViaggio = calcoloViaggio - calcoloViaggio20;
+      console.log(calcoloViaggioSconto20);
+      offerta = 'Sconto Minorenne';
+    }
+    else if (fascia == 'Over65') {
+
+      calcoloViaggio = calcoloViaggio - calcoloViaggio40;
+      console.log(calcoloViaggioSconto40);
+      offerta = 'Sconto Silver';
+    }
+    else {
+
+      calcoloViaggio
+      console.log(calcoloViaggio)
+    }
+
+    var carrozza = Math.floor(Math.random() * 9) + 1;
+    var cp = Math.floor(Math.random() * (100000 - 90000 + 1) ) + 90000;
+    document.getElementById('nome_utente').innerHTML = nome;
+    document.getElementById('offerta').innerHTML = offerta;
+    document.getElementById('costo').innerHTML = calcoloViaggio.toFixed(2);
+    document.getElementById('carrozza').innerHTML = carrozza;
+    document.getElementById('codice-cp').innerHTML = cp;
   }
 );
